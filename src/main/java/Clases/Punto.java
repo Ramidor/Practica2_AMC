5dff772fbc17ba7f65e4892f4a43cb62b5bbd87b
raulp <raulp@DESKTOP-9IMU10S>
2024-12-03 11:08:36 +0100

    intento voraz

--- src/main/java/Clases/Punto.java
+++ src/main/java/Clases/Punto.java
@@ -8,7 +8,6 @@ import java.util.Random;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author raulp
@@ -18,69 +17,38 @@ public class Punto {
     private double x;
     private double y;
     private int id;
-    
-    public Punto(){
+
+    public Punto() {
         x = 0;
         y = 0;
     }
-    
-    public Punto(int id ,double x, double y){
+
+    public Punto(int id, double x, double y) {
         this.id = id;
         this.x = x;
         this.y = y;
     }
-    
-    public static double distancia(Punto a, Punto b){
-        double d = Math.sqrt((b.x-a.x)*(b.x-a.x)+(b.y-a.y)*(b.y-a.y));
-        return d;
-    }
-    
-    public double getX(){
+
+    public double getX() {
         return x;
     }
-    
-    public double getY(){
+
+    public double getY() {
         return y;
     }
-    
-    public int getId(){
+
+    public int getId() {
         return id;
     }
-    
-    public void setX(double x){
+
+    public void setX(double x) {
         this.x = x;
     }
-    public void setY(double y){
+
+    public void setY(double y) {
         this.y = y;
     }
-    
-    public void rellenarPuntos(List<Punto> p, int n, boolean mismax) {
-        int num, den;
-        double x, y, aux1;
-        Random r = new Random();
-        r.setSeed(System.currentTimeMillis()); // initialize random seed:
-        if (mismax) { //PEOR CASO
-            for (int i = 0; i < n; i++) {
-                aux1 = r.nextDouble(1000)+7; //7 y 1007
-                y = aux1 / ((double) i + 1 + i * 0.100); //aux2; //+(i/3.0);
-                num = r.nextInt(3);
-                y += ((i % 500) - num * (r.nextDouble(100)));
-                x = 1;
-                Punto punto = new Punto(i+1,x,y);
-                p.add(punto);
-            }
-        } else { //CASO MEDIO
-            for (int i = 0; i < n; i++) {
-                num = r.nextInt(4000)+1; //genera un número aleatorio entre 1 y 4000
-                den = r.nextInt(11)+7; //genera un aleatorio entre 7 y 17
-                x = num / ((double) den + 0.37); //division con decimales
-                y = (r.nextInt(4000)+1) / ((double) (r.nextInt(11)+7) + 0.37);
-                Punto punto = new Punto(i+1,x,y);
-                p.add(punto);
-            }
-        }
-    }
-    
+
     @Override
     public String toString() {
         return "Punto{Id= " + id + ", x=" + x + ", y=" + y + '}';
--- src/main/java/Clases/Voraces.java
+++ src/main/java/Clases/Voraces.java
@@ -4,74 +4,97 @@
  */
 package Clases;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author raulp
  */
 public class Voraces {
 
-    public int[] vorazuni(puntos p[], int ini){
-        boolean visitados = new boolean[p.lenght()];//inicialiazar con false todas las celdas con un for
-        int ruta = new int[p.lenght+1];
+    public ArrayList<Punto> vorazuni(ArrayList<Punto> p, int ini) {
+        ArrayList<Punto> visitados = new ArrayList<>();//inicialiazar con false todas las celdas con un for
+        ArrayList<Integer> ruta = new ArrayList<>();
+        visitados.sort((p1, p2) -> Double.compare(p1.getX(), p2.getX()));
+        visitados.
         ruta[0] = ini;
         visitados[ini] = true;
-        for(int i = 1; i<p.lenght; i++){
+        for (int i = 1; i < p.length; i++) {
             ini = ciudadMasCercana(p, visitados, ini);//ini ciudad mas cercana
             ruta[i] = ini;
             visitados[ini] = true;
         }
         ruta[i] = ruta[0];
-        
+
         return ruta;
     }
-    
-        public int[] vorazuniPoda(puntos p[], int ini){
+
+    public int[] vorazuniPoda(puntos p[], int ini) {
         Punto pini = p[ini];
         //ordenar p por eje x
         ini = buscar(p, pini);
         boolean visitados = new boolean[p.lenght()];//inicialiazar con false todas las celdas con un for
-        int ruta = new int[p.lenght+1];
+        int ruta = new int[p.lenght + 1];
         ruta[0] = ini;
         visitados[ini] = true;
-        for(int i = 1; i<p.lenght; i++){
+        for (int i = 1; i < p.lenght; i++) {
             ini = ciudadMasCercanaPoda(p, visitados, ini);//ini ciudad mas cercana
             ruta[i] = ini;
             visitados[ini] = true;
         }
         ruta[i] = ruta[0];
-        
+
         return ruta;
     }
-    
-    public int ciudadMasCercana(puntos p[], boolean v[], int ini){
-        double dist ,dmin = Double.MAX_VALUE;
+
+    public int ciudadMasCercana(puntos p[], boolean v[], int ini) {
+        double dist, dmin = Double.MAX_VALUE;
         int d = -1;
-        for(int i = 0; i<p.length(); i++){
-            if(i != ini && v[i] == false){
-               dist =  distancia(p[ini], p[i]);
-               if(dist<dmin){
-                   dmin=dist;
-                   d = i;
-               }
+        for (int i = 0; i < p.length(); i++) {
+            if (i != ini && v[i] == false) {
+                dist = distancia(p[ini], p[i]);
+                if (dist < dmin) {
+                    dmin = dist;
+                    d = i;
+                }
             }
         }
         return d;
     }
-    
-    
+
     //debe buscar a derecha e izquierda, adaptar el poda que tenemos
-    public int ciudadMasCercanaPoda(puntos p[], boolean v[], int ini){
-        double dist ,dmin = Double.MAX_VALUE;
+    public int ciudadMasCercanaPoda(puntos p[], boolean v[], int ini) {
+        double dist, dmin = Double.MAX_VALUE;
         int d = -1;
-        for(int i = 0; i<p.length(); i++){
-            if(i != ini && v[i] == false){
-               dist =  distancia(p[ini], p[i]);
-               if(dist<dmin){
-                   dmin=dist;
-                   d = i;
-               }
+        for (int i = 0; i < p.length(); i++) {
+            if (i != ini && v[i] == false) {
+                dist = distancia(p[ini], p[i]);
+                if (dist < dmin) {
+                    dmin = dist;
+                    d = i;
+                }
             }
         }
         return d;
+    }
+
+    public ArrayList<Punto> rellenarPuntos(int n) {
+        ArrayList<Punto> p = new ArrayList<>();
+        int num, den;
+        double x, y, aux1;
+        Random r = new Random();
+        r.setSeed(System.currentTimeMillis()); // initialize random seed:
+
+        for (int i = 0; i < n; i++) {
+            num = r.nextInt(4000) + 1; //genera un número aleatorio entre 1 y 4000
+            den = r.nextInt(11) + 7; //genera un aleatorio entre 7 y 17
+            x = num / ((double) den + 0.37); //division con decimales
+            y = (r.nextInt(4000) + 1) / ((double) (r.nextInt(11) + 7) + 0.37);
+            Punto punto = new Punto(i + 1, x, y);
+            p.add(punto);
+        }
+
+        return p;
     }
 }
